f50c829ea5439688efdb61d2c7ca7021386dac01
adu <cryoutsolutions@gmail.com>
2014-02-27 11:05:46 +0200

    added config

---
+++ src/Cryoutsolutions/paypalrecuringpaymentscreditcard/PaypalRecuringPaymentsCreditCard.php
@@ -0,0 +1,16 @@
+<?php namespace Cryoutsolutions\dprp;
+
+
+class PaypalRecuringPaymentsCreditCard{
+  
+
+    // Class functions
+
+    public static function Address()
+    {
+        return new Address();
+    }
+
+    
+
+}
---
+++ src/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+return array(
+    'mode' => 'sandbox',
+    'acct1.UserName' => 'pbsandbox_api1.cryoutsolutions.com',
+    'acct1.Password' => '1393364677',
+    'acct1.Signature' => 'A0naXXmvthc892Q-Euq4e1Yeq9DbAUz4FUCj-41rVY-DilLCUfQCuRPB'
+);
