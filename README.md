3be8dcf086fd45d83afa0407d8704ac22673c667
adu <cryoutsolutions@gmail.com>
2014-02-28 12:14:06 +0200

    first working version

---
+++ README.md
@@ -0,0 +1,58 @@
+laravel-paypalrecuringpaymentscreditcard
+=====================
+
+laravel-paypalrecuringpaymentscreditcard 
+process  direct credit card payments recurring porofiles with laravel 4
+Installation
+=============
+Install this package through Composer. To your `composer.json` file, add:
+
+```js
+"require-dev": {
+    "cryoutsolutions/paypalpayment": "dev-master"
+}
+```
+
+Next, run `composer update` to download it.
+
+Add the service provider to `app/config/app.php`, within the `providers` array.
+
+```php
+'providers' => array(
+    // ...
+
+    'Cryoutsolutions\Paypalpayment\PaypalpaymentServiceProvider',
+)
+```
+
+Finally, add the alias to `app/config/app.php`, within the `aliases` array.
+
+```php
+'aliases' => array(
+    // ...
+
+    'Paypalpayment'   => 'Cryoutsolutions\Paypalpayment\Facades\PaypalPayment',
+)
+```
+##Configuration
+php artisan config:publish cryoutsolutions/paypalpayment
+
+Example Code
+============
+
+##1-Create Recurring Profile 
+
+    $startDate= "2014-02-28T00:00:00:000Z";
+    $frequency = 1;
+    $period = "Month";
+    $amount=1;
+    $initialAmount=1;
+    $creditCard = Paypalpayment::CreditCardDetailsType();
+    $creditCard->CreditCardNumber = "4745425765192217";
+    $creditCard->CreditCardType = "Visa";
+    $creditCard->CVV2 = "962";
+    $creditCard->ExpMonth = 02;
+    $creditCard->ExpYear = 2014;
+    Paypalpayment::CreateRecurringProfile($startDate,$frequency,$period,$amount,$initialAmount,$creditCard);
+
+Sponsored by therapick.com
--- src/Cryoutsolutions/Paypalpayment/PaypalPayment.php
+++ src/Cryoutsolutions/Paypalpayment/PaypalPayment.php
@@ -1,14 +1,86 @@
 <?php namespace Cryoutsolutions\Paypalpayment;
 
+use ActivationDetailsType;
+use BasicAmountType;
+use BillingPeriodDetailsType;
+use CreateRecurringPaymentsProfileReq;
+use CreateRecurringPaymentsProfileRequestDetailsType;
+use CreateRecurringPaymentsProfileRequestType;
+use CreditCardDetailsType;
+use Illuminate\Support\Facades\Config;
+use PayPalAPIInterfaceServiceService;
+use RecurringPaymentsProfileDetailsType;
+use ScheduleDetailsType;
 
 class PaypalPayment{
   
 
     // Class functions
 
-    public static function Address()
+    public static function CreateRecurringProfile($startDate,$frequency,$period,$amount,$initialAmount,$creditCard)
     {
-        return TRUE;
+        
+        $profileDetails = PaypalPayment::RecurringPaymentsProfileDetailsType();
+        $profileDetails->BillingStartDate = $startDate;
+
+        $paymentBillingPeriod = PaypalPayment::BillingPeriodDetailsType();
+        $paymentBillingPeriod->BillingFrequency = $frequency;
+        $paymentBillingPeriod->BillingPeriod = $period;
+        $paymentBillingPeriod->Amount = PaypalPayment::BasicAmountType("USD", $amount);
+        $actDetails=PaypalPayment::ActivationDetailsType();
+        $actDetails->InitialAmount=PaypalPayment::BasicAmountType("USD",$initialAmount);
+        $scheduleDetails = PaypalPayment::ScheduleDetailsType();
+        $scheduleDetails->ActivationDetails=$actDetails;
+        $scheduleDetails->Description = "recurring billing";
+        $scheduleDetails->PaymentPeriod = $paymentBillingPeriod;
+
+        $createRPProfileRequestDetails = PaypalPayment::CreateRecurringPaymentsProfileRequestDetailsType();
+        $createRPProfileRequestDetails->CreditCard = $creditCard;
+
+        $createRPProfileRequestDetails->ScheduleDetails = $scheduleDetails;
+        $createRPProfileRequestDetails->RecurringPaymentsProfileDetails = $profileDetails;
+
+        $createRPProfileRequest = PaypalPayment::CreateRecurringPaymentsProfileRequestType();
+        $createRPProfileRequest->CreateRecurringPaymentsProfileRequestDetails = $createRPProfileRequestDetails;
+
+        $createRPProfileReq = PaypalPayment::CreateRecurringPaymentsProfileReq();
+        $createRPProfileReq->CreateRecurringPaymentsProfileRequest = $createRPProfileRequest;
+
+        
+        $paypalService = PaypalPayment::PayPalAPIInterfaceServiceService();
+        $createRPProfileResponse = $paypalService->CreateRecurringPaymentsProfile($createRPProfileReq);
+        return $createRPProfileResponse;
+    }
+    public static function ActivationDetailsType() {
+        return new ActivationDetailsType();
+    }
+    public static function BasicAmountType($currency,$value) {
+        return new BasicAmountType($currency,$value);
+    }
+    public static function BillingPeriodDetailsType() {
+        return new BillingPeriodDetailsType();
+    }
+    public static function CreateRecurringPaymentsProfileReq() {
+        return new CreateRecurringPaymentsProfileReq();
+    }
+    public static function CreateRecurringPaymentsProfileRequestDetailsType() {
+        return new CreateRecurringPaymentsProfileRequestDetailsType();
+    }
+    public static function CreateRecurringPaymentsProfileRequestType() {
+        return new CreateRecurringPaymentsProfileRequestType();
+    }
+    public static function CreditCardDetailsType() {
+        return new CreditCardDetailsType();
+    }
+    public static function PayPalAPIInterfaceServiceService() {
+        $config = Config::get('paypalpayment::config');
+        return new PayPalAPIInterfaceServiceService($config);
+    }
+    public static function RecurringPaymentsProfileDetailsType() {
+        return new RecurringPaymentsProfileDetailsType();
+    }
+    public static function ScheduleDetailsType() {
+        return new ScheduleDetailsType();
     }
 
 }
